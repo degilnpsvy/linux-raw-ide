21e9778aa014ff3b111782d38ab1fb9f7bf0598d
TangXC <tangxch@foxmail.com>
2016-04-17 09:58:49 +0800

    fix repo path
    Signed-off-by: TangXC <tangxch@foxmail.com>

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ rm -rf .git
 git clone https://github.com/ossxp-com/repo
 rm -rf ~/.repoconfig
 ./repo/repo init -u https://github.com/degilnpsvy/manifest.git
-repo sync
+./repo/repo sync
 rm start.sh
